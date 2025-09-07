a1c6b77c1ac00bd6dc17fee62dd73b9f53293994
Teja L <teja.louta@gmaill.com>
2025-09-07 10:45:00 +0300

    Sign in page button styling

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -10,10 +10,16 @@ const SigninPage = () => {
     console.log("Data: ", data);
   };
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center w-[40vw] h-full">
       <div className="flex w-full h-full justify-center items-center">
-        <span>You need to use your Reddit account to Authorise this app.</span>
-        <button onClick={handleLogin}> Click </button>
+        <span>You need to use your Reddit account to Authorise this app. </span>
+        <button
+          onClick={handleLogin}
+          className="bg-pink-900 rounded-[16px] cursor-pointer h-10 w-10"
+        >
+          {" "}
+          Click{" "}
+        </button>
       </div>
     </div>
   );
